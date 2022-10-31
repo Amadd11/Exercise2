df54810d0d77ed9ae028f0d85a5ecba04d81a023
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-31 16:46:10 +0700

    Add Method Displayy in merge sort

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -108,7 +108,30 @@ namespace Exercise2
                 MergeSort(numbers, left, (mid + 1), right);
             }
         }
-
+        public void Displayy() 
+        {
+            Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+            Console.Write("\n\nEnter number of elements: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.Write("Input int array : ");
+            Console.Write("\n");
+            for (int k = 0; k < max; k++)
+            {
+                Console.Write(numbers[k] + " ");
+                Console.Write("\n");
+            }
+            Console.WriteLine("MergeSort By Recursive Method");
+            SortMergeArray(numbers, 0, max - 1);
+            for (int i = 0; i < max; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadLine();
+        }
 
         static void Main(string[] args)
         {
