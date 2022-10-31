76881a4224e1470d28316f5ec910c52d520f381b
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-31 15:49:17 +0700

    Add Array of interger

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,10 +8,14 @@ namespace Exercise2
 {
     class QuickShort
     {
-        //
+        //array of interger to hold values
         private int[] mss = new int[62];
-        private int[] cmp_count = 0;
-        private int[] mov_count = 0;
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        //number od elements in array
+        private int n;
+      
         static void Main(string[] args)
         {
         }
