320471ab8d95dd1b9bab1387089a8437a32ac756
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-31 15:45:03 +0700

    Add project files.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class QuickShort
+    {
+        //
+        private int[] mss = new int[62];
+        private int[] cmp_count = 0;
+        private int[] mov_count = 0;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
