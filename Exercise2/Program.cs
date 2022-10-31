8b3bf461a8f843ef1161f24aff434864a62ca368
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-31 16:16:37 +0700

    Add method Read

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace Exercise2
 {
-    class QuickShort
+    class BubbleSort
     {
-        //array of interger to hold values
-        private int[] mss = new int[62];
-        private int cmp_count = 0;
-        private int mov_count = 0;
+        //deklarasi array int dengan ukuran 20
+        private int[] a = new int[52];
 
-        //number od elements in array
+        //Deklarasi variable int untuk menyimpan banyaknya data pada array
         private int n;
-      
+
+        // Fungsi / method atau menerima masukan
+        public void Read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukkan Banyak Elemen pada Array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 52)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunya maksimal 52 elemen.\n");
+
+            }
+        }
         static void Main(string[] args)
         {
         }
